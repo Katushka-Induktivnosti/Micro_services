798b25a1a5eed916fe803d8f022ce1f907468344
Katushka-Induktivnosti <70918751+Katushka-Induktivnosti@users.noreply.github.com>
2022-04-05 16:05:44 +0300

    Add files via upload

---
+++ message_service.py
@@ -0,0 +1,11 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/messages', methods=['POST','GET'])
+def message():
+    return "Service was not implemented yet"
+
+app.run(host='0.0.0.0', port='8081')
