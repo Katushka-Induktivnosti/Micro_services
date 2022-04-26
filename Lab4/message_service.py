07bee98ef699c39b112d7814017fbf9054c8d3b8
Katushka-Induktivnosti <70918751+Katushka-Induktivnosti@users.noreply.github.com>
2022-04-26 11:36:47 +0300

    Update message_service.py

--- Lab4/message_service.py
+++ Lab4/message_service.py
@@ -5,41 +5,44 @@ import time
 import os
 import threading
 
+msg = []
+msg_lock = threading.Lock()
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-msg = []
 
 @app.route('/messages', methods=['GET'])
 
 def message():
-    data=flask.request.json['msg']
-    def ret_func():
-        if flask.request.method == 'GET':
-            return ','.join(msg)
-    def queue():
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        channel = connection.channel()
-        channel.queue_declare(queue='lab4')
+    global msg
+    if len(msg) < 5:
+        return "Not enough messages"
+    with msg_lock:
+        wiped_msg = msg.copy()
+        msg.clear()    
+    return ','.join(wiped_msg)
 
-        def callback(ch, method, properties, body):
-            print(" [x] Received %r" % body.decode())
-            msg.append(body.decode())
-            print (','.join(msg))
-
-        channel.basic_consume(queue='lab4', on_message_callback=callback, auto_ack=True)
-
-        print(' [*] Waiting for messages. To exit press CTRL+C')
-        channel.start_consuming()
-
-    t1 = threading.Thread(target = queue)
-    t1.start()
-
-    t2 = threading.Thread(target=ret_func)
-    t2.start()
-    t2.join()
-    return ','.join(msg)
+    return ','.join(wiped_msg)
     
+def queue():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='lab4')
+
+    def callback(ch, method, properties, body):
+        global msg
+        with msg_lock:
+            msg.append(body.decode())
+        print(" [x] Received %r" % body.decode())
+        print (','.join(msg))
+
+    channel.basic_consume(queue='lab4', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
 if __name__ == '__main__':
+    queue_thread = threading.Thread(target = queue)
+    queue_thread.start()
     app.run(host='0.0.0.0', port='81')
     
